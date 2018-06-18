468841537bbb942a83f820e2689f2cdb1742e515
Nathan Merritt <gnm@the-merritts.net>
2018-06-18 12:55:22 -0400

    add production django settings

---
+++ src/outreach/prod_settings.py
@@ -0,0 +1,27 @@
+import os
+from .settings import *  # noqa F403
+
+DEBUG = False
+DEBUG_TEMPLATE = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+ALLOWED_HOSTS = [".sciencefor2020.org"]
+
+INSTALLED_APPS += [  # noqa F405
+    'gunicorn',
+]
+
+DATABASES['default']['PASSWORD'] = os.environ['PG_PASS']  # noqa F405
+
+static_dir = os.environ.get('STATIC_ROOT', None)
+if static_dir is not None:
+    STATIC_ROOT = static_dir
+
+MIDDLEWARE.remove('querycount.middleware.QueryCountMiddleware')  # noqa F405
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
--- src/start.sh
+++ src/start.sh
@@ -2,8 +2,13 @@
 set -e
 cd "$(dirname "$0")"
 
-python manage.py makemigrations
-python manage.py migrate
-gunicorn outreach.wsgi \
+HOST_IP=`ifconfig eth0 | awk '/t addr:/{gsub(/.*:/,"",$2);print$2}'`
+
+source PROD_ENV.sh
+
+python manage.py --settings=outreach.prod_settings migrate
+gunicorn --env DJANGO_SETTINGS_MODULE=outreach.prod_settings
   --workers 2 \
-  -b 0.0.0.0:8000
+  --pid outreach-gunicorn.pid \
+  -b ${HOST_IP}:8000 \
+  outreach.wsgi
