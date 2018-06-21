927844fbfbe61c6e9eecc9c0cdebba4422aeec12
Nathan Merritt <gnm@the-merritts.net>
2018-06-21 17:55:28 -0400

    basic info added to index

--- src/programs/urls.py
+++ src/programs/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('programs/', views.ProgramList.as_view(), name='programs'),
     path('program/<int:pk>/', views.Program.as_view(), name='program'),  # TODO: name into Url
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', views.Index.as_view(), name='index'),
 ]
--- src/programs/views.py
+++ src/programs/views.py
@@ -1,6 +1,18 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import TemplateView, DetailView, ListView
 
 from .models import Program
+
+
+class Index(TemplateView):
+    template_name = "index.html"
+
+    def topics(self):
+        return ['STEM', 'Science', 'Engineering']  # TODO
+        return Program.objects.only('topic').all()
+
+    def age_groups(self):
+        return ['2nd grade', 'third grade', '5-8th grades']  # TODO
+        return Program.objects.only('age_group').all()
 
 
 class ProgramList(ListView):
