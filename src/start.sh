e09811d7dfee73ddfed017050668ba8971e89292
Nathan Merritt <gnm@the-merritts.net>
2018-06-21 14:08:35 -0400

    fixed typo in start.sh

--- src/start.sh
+++ src/start.sh
@@ -8,7 +8,8 @@ source ../venv/bin/activate
 source PROD_ENV.sh
 
 python manage.py migrate --settings=outreach.prod_settings
-gunicorn outreach.wsgi --env DJANGO_SETTINGS_MODULE=outreach.prod_settings
+gunicorn --env DJANGO_SETTINGS_MODULE=outreach.prod_settings \
   --workers 2 \
   --pid outreach-gunicorn.pid \
-  -b ${HOST_IP}:8000
+  -b ${HOST_IP}:8000 \
+  outreach.wsgi
