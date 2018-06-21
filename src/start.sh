bd61ded487b288aff668c8cf5299e9f784519628
Nathan Merritt <gnm@the-merritts.net>
2018-06-21 16:10:41 -0400

    moved django settings to environment variable

--- src/start.sh
+++ src/start.sh
@@ -7,8 +7,8 @@ HOST_IP=`/sbin/ifconfig eth0 | awk '/t addr:/{gsub(/.*:/,"",$2);print$2}'`
 source ../venv/bin/activate
 source PROD_ENV.sh
 
-python manage.py migrate --settings=outreach.prod_settings
-gunicorn --env DJANGO_SETTINGS_MODULE=outreach.prod_settings \
+python manage.py migrate
+gunicorn \
   --workers 2 \
   --pid outreach-gunicorn.pid \
   -b ${HOST_IP}:8000 \
